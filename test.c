bb6d11e96ae11720c32edbaf332bc9163a49b3bb
tomas-20 <tacuna20@stuy.edu>
2021-10-18 10:18:28 -0400

    created a test example of the new functions

--- test.c
+++ test.c
@@ -1,2 +1,9 @@
 #include <stdio.h>
-int main() {}
+#include "pokemon.h"
+int main() {
+  struct pokemon *listo = NULL;
+  insert_front(listo, "Weedle", "Bug/Poison");
+  insert_front(listo, "Eevee", "Normal");
+  insert_front(listo, "Goomy", "Dragon");
+  print_list(listo);
+}

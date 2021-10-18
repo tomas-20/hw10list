8f04595d45e2341fd54eb8678351cf1ac53132ba
tomas-20 <tacuna20@stuy.edu>
2021-10-18 17:11:26 -0400

    updated test.c to reflect the brighter times

--- test.c
+++ test.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
 #include "pokemon.h"
 int main() {
-  struct pokemon *single = insert_front(NULL, "Weedle", "Bug/Poison");
-  struct pokemon *doble = insert_front(single, "Eevee", "Normal");
-  struct pokemon *triple = insert_front(doble, "Goomy", "Dragon");
-  printf("\n");
-  printf("%p should be the same as %p\n", doble, triple->next);
-  printf("%p should be the same as %p\n", single, doble->next);
+  struct pokemon *listo = NULL;
+  listo = insert_front(listo, "Weedle", "Bug/Poison");
+  listo = insert_front(listo, "Eevee", "Normal");
+  listo = insert_front(listo, "Goomy", "Dragon");
+  print_list(listo);
 }

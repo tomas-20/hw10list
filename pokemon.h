d4a9d1797416b2fcc789025e421c7285e1f74171
tomas-20 <tacuna20@stuy.edu>
2021-10-18 20:38:54 -0400

    changed the position of catch_pokemon in pokemon.h

--- pokemon.h
+++ pokemon.h
@@ -4,7 +4,7 @@ struct pokemon {
   struct pokemon *next;
 };
 void whos_that_pokemon(struct pokemon *);
-struct pokemon *catch_pokemon(char *, char *);
 void print_list(struct pokemon *);
+struct pokemon *catch_pokemon(char *, char *);
 struct pokemon *insert_front(struct pokemon *, char *, char *);
 struct pokemon *free_list(struct pokemon *);
